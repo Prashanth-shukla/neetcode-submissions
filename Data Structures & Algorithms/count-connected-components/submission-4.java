0f91961405389469e0ddb5b9540d53bb7c7e4cb5
Prashanth-Shukla <147225621+Prashanth-shukla@users.noreply.github.com>
2026-07-13 21:51:17 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int edge[] : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        int[] rank = new int[n];
+        int count = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int curr : adj.get(i)) {
+                union(i, curr, parent, rank);
+             
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : parent) set.add(find(i, parent));
+        return set.size();
+    }
+
+    public int find(int x, int[] parent) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x], parent);
+    }
+
+    public void union(int x, int y, int[] parent, int[] rank) {
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+        if (x_parent == y_parent)
+            return;
+
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        }
+
+        else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        }
+
+        else {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+}
